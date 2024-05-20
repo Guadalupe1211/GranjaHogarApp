27f0cc0c6d1464a3677e82239afaf851dcbe9d8c
Guadalupe1211 <A01563124@tec.mx>
2024-05-19 21:45:45 -0600

    Formulario oculto de categoria. ARREGLAR ACTUALIZACION INMEDIATA

--- appgranja/src/Componentes/CategoriasTabla.js
+++ appgranja/src/Componentes/CategoriasTabla.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Cambia useHistory a useNavigate
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import * as CategoriaService from '../Services/CategoriaService';
 import * as CategoriaServiceCreate from '../Services/CategoriaServiceCreate';
-import '../CategoriasGaleria.css';
 
-export const CategoriasGaleria = () => {
+export const CategoriasGaleria = ({ onEditCategoria }) => {
     const [categorias, setCategorias] = useState([]);
-    const [categoriaSeleccionada, setCategoriaSeleccionada] = useState({ id: '', nombre: '', descripcion: '' });
-    const navigate = useNavigate(); // Cambia useHistory a useNavigate
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,30 +22,8 @@ export const CategoriasGaleria = () => {
         fetchData();
     }, []);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        
-        try {
-            const id = categoriaSeleccionada.id;
-            const aux = categoriaSeleccionada;
-            delete aux.id;
-            if (id) {
-                await CategoriaServiceCreate.updateCategoria(id, aux);
-                alert('Categoría actualizada correctamente');
-            } else {
-                await CategoriaServiceCreate.createCategoria(categoriaSeleccionada);
-                alert('Categoría creada correctamente');
-            }
-            const categoriasData = await CategoriaService.getCategorias();
-            setCategorias(categoriasData);
-            setCategoriaSeleccionada({ id: '', nombre: '', descripcion: '' });
-        } catch (error) {
-            console.error('Error al guardar la categoría:', error);
-        }
-    };
-
     const handleUpdate = (categoria) => {
-        setCategoriaSeleccionada(categoria);
+        onEditCategoria(categoria);
     };
 
     const handleDelete = async (id) => {
@@ -60,43 +36,12 @@ export const CategoriasGaleria = () => {
         }
     };
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setCategoriaSeleccionada((prevState) => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-
     const handleCategoryClick = (categoriaId) => {
-        navigate(`/productos/${categoriaId}`); // Cambia history.push a navigate
+        navigate(`/productos/${categoriaId}`);
     };
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Nombre:</label>
-                    <input
-                        type="text"
-                        name="nombre"
-                        value={categoriaSeleccionada.nombre}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Descripción:</label>
-                    <textarea
-                        name="descripcion"
-                        value={categoriaSeleccionada.descripcion}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">{categoriaSeleccionada.id ? 'Actualizar Categoría' : 'Crear Categoría'}</button>
-            </form>
-
             <div className="galeria">
                 {categorias.map((categoria) => (
                     <div key={categoria.id} className="categoria-card" onClick={() => handleCategoryClick(categoria.id)}>
--- appgranja/src/Paginas/Categorias.js
+++ appgranja/src/Paginas/Categorias.js
@@ -1,23 +1,109 @@
-// CategoriasPage.js
-import React from 'react';
-//import CategoriasTabla from './CategoriasTabla';
-//import CategoriasTabla from './Componentes/CategoriasTabla';
+import React, { useState, useEffect  } from 'react';
 import CategoriasGaleria from '../Componentes/CategoriasTabla';
-import CreateCategorias from '../Componentes/CreateTabla';
-
-
+import * as CategoriaServiceCreate from '../Services/CategoriaServiceCreate';
+import '../CategoriasGaleria.css';
+import '../Categorias.css';
+import * as CategoriaService from '../Services/CategoriaService';
 const CategoriasPage = () => {
+    const [showForm, setShowForm] = useState(false);
+    const [categoriaSeleccionada, setCategoriaSeleccionada] = useState({ id: '', nombre: '', descripcion: '' });
+    const [categorias, setCategorias] = useState([]);
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const categoriasData = await CategoriaService.getCategorias();
+            setCategorias(categoriasData);
+        } catch (error) {
+            console.error('Error al obtener las categorías:', error);
+        }
+    };
+
+    const toggleFormVisibility = () => {
+        setShowForm(prevShowForm => !prevShowForm);
+    };
+
+    const handleCategoriaGuardada = async () => {
+        setShowForm(false);
+        await fetchData();
+        // Recargar las categorías después de guardar
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const id = categoriaSeleccionada.id;
+            const aux = categoriaSeleccionada;
+            delete aux.id;
+            if (id) {
+                await CategoriaServiceCreate.updateCategoria(id, aux);
+                alert('Categoría actualizada correctamente');
+            } else {
+                await CategoriaServiceCreate.createCategoria(categoriaSeleccionada);
+                alert('Categoría creada correctamente');
+            }
+            // Recargar las categorías después de guardar
+            handleCategoriaGuardada();
+        } catch (error) {
+            console.error('Error al guardar la categoría:', error);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setCategoriaSeleccionada((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleEditCategoria = (categoria) => {
+        setCategoriaSeleccionada(categoria);
+        setShowForm(true);
+    };
+
     return (
         <div>
-            <h1>Categorias</h1>
-            <CategoriasGaleria />
-            <CreateCategorias />
-    
-    
-
+            <h1>Categorías</h1>
+            <div className="button-container">
+                <button
+                    className="btn btn-primary"
+                    onClick={toggleFormVisibility}
+                >
+                    {showForm ? 'Ocultar Formulario' : 'Agregar Categoría'}
+                </button>
+            </div>
+            {showForm && (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>Nombre:</label>
+                        <input
+                            type="text"
+                            name="nombre"
+                            value={categoriaSeleccionada.nombre}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label>Descripción:</label>
+                        <textarea
+                            name="descripcion"
+                            value={categoriaSeleccionada.descripcion}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <button type="submit">{categoriaSeleccionada.id ? 'Actualizar Categoría' : 'Crear Categoría'}</button>
+                </form>
+            )}
+            <CategoriasGaleria onEditCategoria={handleEditCategoria} />
         </div>
     );
 };
 
 export default CategoriasPage;
-
