b92fec9b7afaebda024045c80fd6aae20c390c1b
Guadalupe1211 <117548332+Guadalupe1211@users.noreply.github.com>
2026-05-13 17:35:05 -0600

    Refactor Django settings for production readiness
    Updated settings to use environment variables for secret key and debug mode. Configured database settings to use dj_database_url for production.

--- SistemaInventario/SistemaInventario/settings.py
+++ SistemaInventario/SistemaInventario/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q$bf&5i@-2=lm$&z=+5l2ydusf&ss&=_$nb2j(p#-!t7+pqs+5"
+# SECRET_KEY = "django-insecure-q$bf&5i@-2=lm$&z=+5l2ydusf&ss&=_$nb2j(p#-!t7+pqs+5"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-q$bf&5i@-2=lm$&z=+5l2ydusf&ss&=_$nb2j(p#-!t7+pqs+5')
+ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -43,23 +48,25 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",        
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',             
     "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST=(
-    'http://localhost:3000',
-)
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST=(
+    #'http://localhost:3000',
+#)
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "SistemaInventario.urls"
+WSGI_APPLICATION = "SistemaInventario.wsgi.application"
 
 TEMPLATES = [
     {
@@ -77,19 +84,30 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "SistemaInventario.wsgi.application"
+# WSGI_APPLICATION = "SistemaInventario.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+ #   "default": {
+  #      "ENGINE": "django.db.backends.sqlite3",
+   #     "NAME": BASE_DIR / "db.sqlite3",
+    #}
+#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=not DEBUG,   # SSL solo en producción (Neon lo requiere)
+    )
 }
 
+#Archvivos estaticos
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorag
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -125,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+#STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
